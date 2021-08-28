30a18baf155e532232a028a854f732db04327835
Jay Jenks <jamesljenks@gmail.com>
2021-08-28 17:03:06 -0400

    Attempted to debug the server.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
 // const routes = require('./routes');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware} = require('./utils/auth');
@@ -10,7 +9,7 @@ const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
 
@@ -19,6 +18,7 @@ const server = new ApolloServer({
   resolvers,
   context: authMiddleware
 });
+
 
 // Applying the Apollo Server to the Express server as middleware.
 server.applyMiddleware({ app });
@@ -40,20 +40,3 @@ db.once('open', () => {
   });
 });
 
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // if we're in production, serve client/build as static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
-
-// app.use(routes);
-
-// db.once('open', () => {
-//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-// });
